26ed4cb0884973a243e1609566826f08f8180f24
kashishpandey09 <pkashish852@gmail.com>
2025-06-16 15:41:26 +0530

    Update resize.py

--- resize.py
+++ resize.py
@@ -3,6 +3,17 @@ import cv2
 import numpy as np
 from PIL import Image
 import io
+hide_streamlit_cloud_elements = """
+    <style>
+    #MainMenu {visibility: hidden;}
+    header {visibility: hidden;}
+    footer {visibility: hidden;}
+    .stDeployButton {display: none;}
+    a[title="View source"] {display: none !important;}
+    button[kind="icon"] {display: none !important;}
+    </style>
+"""
+st.markdown(hide_streamlit_cloud_elements, unsafe_allow_html=True)
 
 st.header("Welcome to Image Resizer")
 
